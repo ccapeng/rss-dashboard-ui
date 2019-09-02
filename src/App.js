a6925339c6d389f206f38dfe547531769983b9ef
apeng <ccapeng@yahoo.com>
2019-09-02 19:53:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState({ items: [] });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      setIsLoading(true);
+      const result = await axios('http://localhost:8083/rss/dashboard',
+        {
+          headers:{"Access-Control-Allow-Origin": "*"}
+        }
+      );
+      setData(result.data);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">
+            News
+          </h3>
+        </div>
+        <div className="panel-body">
+          {isLoading ? (
+            <div>Loading</div>
+          ) : (
+            <ul className="list-group">
+              {data.map(item =>
+                <li key="{item.id}" className="list-group-item mt-3">
+                  <div>
+                    <a target="_blank" href={item.url} rel="noopener noreferrer">
+                      {item.title}
+                    </a>
+                  </div>
+                  <span className="small">
+                    {item.publishedAt}
+                  </span>
+                  <div className="mt-2" dangerouslySetInnerHTML={{__html: item.description}} />
+                </li>
+              )}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
